ad919599b4bcb3d3b192005418e8f27dd550a2ab
cakl <sebastian.bock@bluewin.ch>
2014-03-17 16:17:23 +0100

    Initial Commit

---
+++ lootr/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  lootr
+//
+//  Created by Sebastian Bock on 17.03.14.
+//  Copyright (c) 2014 Hochschule Rapperswil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
