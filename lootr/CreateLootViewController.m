1cb2c1c603854e9a9c55a02d8be665fb77ea2e54
cakl <sebastian.bock@bluewin.ch>
2014-04-16 00:35:38 +0200

    refactoring some unused comments

--- lootr/CreateLootViewController.m
+++ lootr/CreateLootViewController.m
@@ -90,16 +90,4 @@
                       onFailure:^(NSError *error) { NSLog(@"%@", error); }];
 }
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little
- preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
- {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
-
 @end
