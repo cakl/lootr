96387b750a331d8c5fb9f25fd9dd4d178170d1e3
cakl <sebastian.bock@bluewin.ch>
2014-04-27 00:23:51 +0200

    open keychain maybe helps

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,5 +3,6 @@ security create-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+open ~/Library/Keychains/ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
