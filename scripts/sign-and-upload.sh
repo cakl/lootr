5eb8bccd70a8ab546f418b0429b63de430df6ed8
cakl <sebastian.bock@bluewin.ch>
2014-04-27 14:11:40 +0200

    testflight integration

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -18,3 +18,22 @@ echo "***************************"
 echo "*        Signing          *"
 echo "***************************"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
+  echo ""
+  echo "***************************"
+  echo "* Uploading to Testflight *"
+  echo "***************************"
+  curl http://testflightapp.com/api/builds.format \
+    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+    -F api_token="$TESTFLIGHT_API_TOKEN" \
+    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+    -F distribution_lists='LootrDistribution' \
+    -F notes="$RELEASE_NOTES"
+fi
