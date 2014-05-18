3fa2829a56f28fb97b71b296351c608f98aae9f4
cakl <sebastian.bock@bluewin.ch>
2014-05-19 01:29:48 +0200

    ContentHeader refactoring

--- lootr/LootContentHeader.m
+++ lootr/LootContentHeader.m
@@ -13,6 +13,18 @@
 @end
 
 @implementation LootContentHeader
+static NSString* const reportButtonImageName = @"ReportButtonIcon";
+static NSString* const lootLabelFont = @"HelveticaNeue-Light";
+static CGFloat const lootLabelXPosition = 10;
+static CGFloat const lootLabelYPosition = 8;
+static CGFloat const lootLabelFontSize = 14;
+static CGFloat const separatorThickness = 0.5;
+static CGFloat const separatorAlpha = 0.3;
+static CGFloat const infoButtonWidth = 40;
+static CGFloat const infoButtonRightMargin = 20;
+static CGFloat const reportButtonRightMargin = 55;
+static CGFloat const reportButtonWidth = 40;
+static CGFloat const reportButtonEdgeInset = 8;
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -20,27 +32,24 @@
     if (self) {
         double width = frame.size.width;
         double height = frame.size.height;
-        self.distanceToLootLabel =  [[UILabel alloc] initWithFrame: CGRectMake(10, 8, width, (height/2))];
-        [self.distanceToLootLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:14]];
+        self.distanceToLootLabel =  [[UILabel alloc] initWithFrame: CGRectMake(lootLabelXPosition, lootLabelYPosition, width, (height/2))];
+        [self.distanceToLootLabel setFont:[UIFont fontWithName:lootLabelFont size:lootLabelFontSize]];
         [self addSubview:self.distanceToLootLabel];
         
-        double separatorThickness = 0.5;
         UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(0, (height-separatorThickness), width, separatorThickness)];
-        separator.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.3];
+        separator.backgroundColor = [UIColor colorWithWhite:0 alpha:separatorAlpha];
         [self addSubview:separator];
         
         self.infoButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-        self.infoButton.frame = CGRectMake(0, 0, 40, height);
-        self.infoButton.center = CGPointMake(width-20, 20);
-        //[self.infoButton addTarget:self action:@selector(infoButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+        self.infoButton.frame = CGRectMake(0, 0, infoButtonWidth, height);
+        self.infoButton.center = CGPointMake(width-infoButtonRightMargin, height/2);
         [self addSubview:self.infoButton];
         
         self.reportButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [self.reportButton setImage:[UIImage imageNamed:@"ReportButtonIcon"] forState:UIControlStateNormal];
-        self.reportButton.frame = CGRectMake(0, 0, 40, height);
-        self.reportButton.imageEdgeInsets = UIEdgeInsetsMake(8, 8, 8, 8);
-        self.reportButton.center = CGPointMake(width-55, 20);
-        //[self.reportButton addTarget:self action:@selector(reportButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+        [self.reportButton setImage:[UIImage imageNamed:reportButtonImageName] forState:UIControlStateNormal];
+        self.reportButton.frame = CGRectMake(0, 0, reportButtonWidth, height);
+        self.reportButton.imageEdgeInsets = UIEdgeInsetsMake(reportButtonEdgeInset, reportButtonEdgeInset, reportButtonEdgeInset, reportButtonEdgeInset);
+        self.reportButton.center = CGPointMake(width-reportButtonRightMargin, height/2);
         [self addSubview:self.reportButton];
     }
     return self;
