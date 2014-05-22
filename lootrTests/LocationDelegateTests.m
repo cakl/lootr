4ca1785a36cb0f384a7bb5b221844f22c21de178
cakl <sebastian.bock@bluewin.ch>
2014-05-23 01:02:40 +0200

    locationdelegate tests

--- lootrTests/LocationDelegateTests.m
+++ lootrTests/LocationDelegateTests.m
@@ -12,9 +12,12 @@
 #import <OCHamcrest/OCHamcrest.h>
 #define MOCKITO_SHORTHAND
 #import <OCMockito/OCMockito.h>
+#import "Errors.h"
 
 @interface LocationDelegateTests : XCTestCase
-
+@property (nonatomic, strong) CLLocationManager* locationManager;
+@property (nonatomic, strong) CLGeocoder* geocoder;
+@property (nonatomic, strong) CoreLocationDelegate* locationDelegate;
 @end
 
 @implementation LocationDelegateTests
@@ -22,22 +25,61 @@
 - (void)setUp
 {
     [super setUp];
-
+    self.locationManager = mock([CLLocationManager class]);
+    self.geocoder = mock([CLGeocoder class]);
+    self.locationDelegate = [[CoreLocationDelegate alloc] initWithLocationManager:self.locationManager geocoder:self.geocoder];
 }
 
 - (void)tearDown
 {
-    
+    self.locationManager = nil;
+    self.geocoder = nil;
+    self.locationDelegate = nil;
     [super tearDown];
 }
 
-- (void)testStartUpdatingLocationSuccess
+- (void)testStartUpdatingLocationJustOnce
 {
+    //when
+    [self.locationDelegate startUpdatingLocation];
+    [self.locationDelegate startUpdatingLocation];
+    //then
+    [verifyCount(self.locationManager, times(1)) startUpdatingLocation];
+}
+
+- (void)testStopUpdatingLocationSuccess
+{
+    //when
+    [self.locationDelegate stopUpdatingLocation];
+    //then
+    [verify(self.locationManager) stopUpdatingLocation];
+}
+
+-(void)testGetCurrentLocationFailure
+{
+    //given
+    NSError* error = nil;
+    //when
+    [self.locationDelegate getCurrentLocationWithError:&error];
+    //then
+    XCTAssertNotNil(error, @"should throw error because of no location");
+    XCTAssertTrue(error.code == locationDeterminationError, @"wrong error was thrown");
+}
+
+-(void)testGetCurrentLocationWithLocationManagerLocationSuccess
+{
+    //given
+    CLLocation* aLocation = [[CLLocation alloc] initWithLatitude:0.0 longitude:0.0];
     CLLocationManager* locationManager = mock([CLLocationManager class]);
     CLGeocoder* geocoder = mock([CLGeocoder class]);
+    [given([locationManager location]) willReturn:aLocation];
     CoreLocationDelegate* locationDelegate = [[CoreLocationDelegate alloc] initWithLocationManager:locationManager geocoder:geocoder];
-    [locationDelegate startUpdatingLocation];
-    [verify(locationManager) startUpdatingLocation];
+    NSError* error = nil;
+    //when
+    CLLocation* returnedLocation = [locationDelegate getCurrentLocationWithError:&error];
+    //then
+    XCTAssertEqualObjects(returnedLocation, aLocation, @"should be the same locations");
 }
+
 
 @end
