23ed11fd3a43c2395139f88bc90b18d0d4675743
cakl <sebastian.bock@bluewin.ch>
2014-05-28 21:49:56 +0200

    testconstants with mockingurl

---
+++ lootrTests/TestConstants.h
@@ -0,0 +1,13 @@
+//
+//  TestConstants.h
+//  lootr
+//
+//  Created by Sebastian Bock on 28.05.14.
+//  Copyright (c) 2014 Hochschule Rapperswil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestConstants : NSObject
+
+@end
---
+++ lootrTests/TestConstants.m
@@ -0,0 +1,13 @@
+//
+//  TestConstants.m
+//  lootr
+//
+//  Created by Sebastian Bock on 28.05.14.
+//  Copyright (c) 2014 Hochschule Rapperswil. All rights reserved.
+//
+
+#import "TestConstants.h"
+
+@implementation TestConstants
+
+@end
